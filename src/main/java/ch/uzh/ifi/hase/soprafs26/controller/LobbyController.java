d4892dc02f1bd61726d4516ca93ba2a4ecd83eee
Gresay <robinmanuel.egger@uzh.ch>
2026-05-10 21:31:23 +0200

    fix

--- src/main/java/ch/uzh/ifi/hase/soprafs26/controller/LobbyController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/controller/LobbyController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -86,7 +87,6 @@ public class LobbyController {
     }
 
     @GetMapping("/lobbies/{lobbyId}")
-    @ResponseStatus(HttpStatus.OK)
     public LobbyGetDTO getLobbyById(
         @PathVariable Long lobbyId,
         @RequestHeader(value = "Authorization", required = false) String authorizationHeader
